db9a6a89fd49693b048f61d3e45e1de58e3b6935
sdrucker66 <sdrucker@stevens.edu>
2021-04-11 23:39:00 -0400

    added index.py

---
+++ index.py
@@ -0,0 +1,24 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if client.user.id != message.author.id:
+        if '-r' in message.content:
+          await message.channel.send('Hi')
+
+
+client.run(token)
