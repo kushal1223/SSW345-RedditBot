9cc810c6e76de554fbf62bb36c556d242f24a226
sdrucker66 <sdrucker@stevens.edu>
2021-04-29 17:22:47 -0400

    created errors for search_post and search_subreddit

--- index.py
+++ index.py
@@ -82,47 +82,66 @@ async def random_post(ctx, query):
 
 @client.command(brief= "<keyword> Returns the top subreddits with the phrase")
 async def search_subreddit(ctx, query):
-
-    #subreddits = await reddit.subreddits(query)
+     #subreddits = await reddit.subreddits(query)
     list = []
     async for subreddit in reddit.subreddits.search(query, limit=10):
         list.append(subreddit)
 
-
-    for i in range(0,10):
-        em = discord.Embed(title = list[i].display_name[0:256], url = "https://www.reddit.com/r/"+list[i].display_name+"/")
-        em.add_field(name = "Subreddit: ", value = list[i].display_name)
-        em.add_field(name = "Subscribers: ", value = list[i].subscribers)
-        if(list[i].public_description != ""):
-            em.add_field(name = "Description:", value = list[i].public_description[0:500], inline= False)
-
+    if not list:
+        em = discord.Embed(title = "Issue")
+        em.add_field(name=" The " + '"'+ query +'"', value='Subreddit was not found', inline= False )
         await ctx.send(embed = em)
+    else: 
+        for i in range(len(list)):
+            em = discord.Embed(title = list[i].display_name[0:256], url = "https://www.reddit.com/r/"+list[i].display_name+"/")
+            em.add_field(name = "Subreddit: ", value = list[i].display_name)
+            em.add_field(name = "Subscribers: ", value = list[i].subscribers)
+            if(list[i].public_description != ""):
+                em.add_field(name = "Description:", value = list[i].public_description[0:500], inline= False)
 
-@client.command(brief= "<keyword> Return a top post containing a keyword")
-async def search_post(ctx, query, subredditname = "all"):
+            await ctx.send(embed = em)
+        
 
+@client.command(brief= "<keyword> Return top posts containing a keyword, to return a specific number of posts, use: 'search_post <keyword> <subreddit> (use all for all of reddit) <number of posts>")
+async def search_post(ctx, query, subredditname = "all", number="10"):
+    num=int(number)
     list_of_rposts = []
-    subreddit = await reddit.subreddit(subredditname)
-    async for submission in subreddit.search(query, limit = 10):
-        list_of_rposts.append(submission)
+    try:
+        subreddit = await reddit.subreddit(subredditname)
+        try:
+            async for submission in subreddit.search(query, limit = num):
+                list_of_rposts.append(submission)
 
-    
+        except:
+            em = discord.Embed(title = "Issue")
+            em.add_field(name=" The " + '"'+ subredditname +'"', value='Subreddit was not found', inline= False )
+            await ctx.send(embed = em)
+            return
 
-    for i in range(0,10):
-        em = discord.Embed(title = list_of_rposts[i].title[0:256], 
+        for i in range(0,num):
+            em = discord.Embed(title = list_of_rposts[i].title[0:256], 
                             url = reddit.config.reddit_url + list_of_rposts[i].permalink, 
                             )
-        em.add_field(name = "Author: ",  value = list_of_rposts[i].author)
-        em.add_field(name = "Number of upvotes: ", value = list_of_rposts[i].score)
-        em.add_field(name = "Subreddit: ", value = list_of_rposts[i].subreddit)
+            em.add_field(name = "Author: ",  value = list_of_rposts[i].author)
+            em.add_field(name = "Number of upvotes: ", value = list_of_rposts[i].score)
+            em.add_field(name = "Subreddit: ", value = list_of_rposts[i].subreddit)
 
-        if(list_of_rposts[i].is_self):
-            em.add_field(name = "Description:", value = list_of_rposts[i].selftext[0:500], inline= False)
-        if (list_of_rposts[i].url[-4:] == '.jpg'):
-            em.set_image(url = list_of_rposts[i].url)
+            if(list_of_rposts[i].is_self):
+                em.add_field(name = "Description:", value = list_of_rposts[i].selftext[0:500], inline= False)
+            if (list_of_rposts[i].url[-4:] == '.jpg' or list_of_rposts[i].url[-4:] ==  '.png'):
+                em.set_image(url = list_of_rposts[i].url)
 
 
+            await ctx.send(embed = em)
+
+
+    except:
+        em = discord.Embed(title = "Issue")
+        em.add_field(name= query +'"', value='was not found in the Subreddit', inline= False )
         await ctx.send(embed = em)
+
+
+
          
 @client.command(brief = "Return a random meme")
 async def meme(ctx):
@@ -145,14 +164,14 @@ async def meme(ctx):
     if(random_sub.is_self):
             em.add_field(name = "Description:", value = random_sub.selftext[0:500], inline= False)
     if (random_sub.url[-4:] == '.jpg'):
-            em.set_image(url = random_sub.url)
+            em.set_image(url = random_sub.url) 
     await ctx.send(embed = em)
 
 @client.command(brief= "Return top 10 posts from all of Reddit")
 async def top(ctx):
     list = []
     subreddit= await reddit.subreddit("all")
-    async for submission in subreddit.top( time_filter= 'day', limit = 11):
+    async for submission in subreddit.top( time_filter= 'day', limit = 10):
         list.append(submission)
     for i in range(0,10):
 
@@ -165,7 +184,7 @@ async def top(ctx):
 
         if(list[i].is_self):
             em.add_field(name = "Description:", value = list[i].selftext[0:500], inline= False)
-        if (list[i].url[-4:] == '.jpg'):
+        if (list[i].url[-4:] == '.jpg' or list[i].url[-4:] =='.png'):
             em.set_image(url = list[i].url)
 
         await ctx.send(embed = em)
@@ -178,7 +197,7 @@ async def help (ctx):
     embed = discord.Embed(title = "Help")
     embed.add_field(name='top', value='Returns the top ten posts of reddit of the day', inline= False )
     embed.add_field(name='meme', value='Returns a meme', inline= False )
-    embed.add_field(name='search_post <keyword> <subreddit (optional)>', value='Returns top ten posts containing the keyword (can be in a specific subreddit)', inline= False )
+    embed.add_field(name='search_post <keyword> <subreddit (optional)> <number (optional)>', value='<keyword> Return top posts containing a keyword, to return a specific number of posts, use: \n "search_post <keyword> <subreddit> (use all for all of reddit) <number of posts>"', inline= False )
     embed.add_field(name='search_subreddit <keyword>', value='Returns the top ten subreddits relating to the keyword', inline= False )
     embed.add_field(name='random_post <subreddit>', value='Returns a random post from a subreddit', inline= False )
 
